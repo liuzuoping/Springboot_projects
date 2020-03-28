3ea73c80fd40eb0f9aeeab202f25b649f9affd03
Jack Liu <2330163321@qq.com>
2020-03-28 21:38:21 +0800

    quickstart

---
+++ springboot_01_quickhelloworld/src/main/java/com/itciaoliu/Springboot01QuickhelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.itciaoliu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01QuickhelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01QuickhelloworldApplication.class, args);
+    }
+
+}
---
+++ springboot_01_quickhelloworld/src/test/java/com/itciaoliu/Springboot01QuickhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itciaoliu;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot01QuickhelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
